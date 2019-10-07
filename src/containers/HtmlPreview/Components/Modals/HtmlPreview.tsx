7392f40db4826e45b07b3fd14f2b97a6751fe8c2
Sinan Temel <sinan.temel@nesbilgi.com.tr>
2019-10-07 18:06:55 +0300

    Tema Seçimi Sayfası Görseli Yapıldı.

--- src/containers/HtmlPreview/Components/Modals/HtmlPreview.tsx
+++ src/containers/HtmlPreview/Components/Modals/HtmlPreview.tsx
@@ -6,7 +6,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { HtmlPreviewStateType } from '../../types';
-
+import { themeFirst } from '../Images/ThemeFirst';
+import { themeFourth } from '../Images/ThemeFourth';
+import { themeSecond } from '../Images/ThemeSecond';
+import { themeThird } from '../Images/ThemeThird';
 interface IProps {
   open: boolean;
   close: () => void;
@@ -31,42 +34,32 @@ class HtmlPreview extends Component<AllProps> {
         <div className="gutter-example">
           <Row gutter={16}>
             <Col className="gutter-row" span={12}>
-              <Card
-                hoverable
-                style={{ width: 512 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
+              <Card hoverable style={{ width: 512 }} cover={<img alt="example" src={themeFirst} />}>
+                <Meta title="1.Tasarım" description="www.nesbilgi.com.tr" />
               </Card>
             </Col>
             <Col className="gutter-row" span={12}>
-              <Card
-                hoverable
-                style={{ width: 512 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
+              <Card hoverable style={{ width: 512 }} cover={<img alt="example" src={themeSecond} />}>
+                <Meta title="2.Tasarım" description="www.nesbilgi.com.tr" />
               </Card>
             </Col>
           </Row>
-          <Row gutter={16}>
+          <Row gutter={16} style={{ marginTop: 10 }}>
             <Col className="gutter-row" span={12}>
-              <Card
-                hoverable
-                style={{ width: 512 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
+              <Card hoverable style={{ width: 512 }} cover={<img alt="example" src={themeThird} />}>
+                <Meta title="3.Tasarım" description="www.nesbilgi.com.tr" />
               </Card>
             </Col>
             <Col className="gutter-row" span={12}>
-              <Card
-                hoverable
-                style={{ width: 512 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-              >
-                <Meta title="Europe Street beat" description="www.instagram.com" />
+              <Card hoverable style={{ width: 512 }} cover={<img alt="example" src={themeFourth} />}>
+                <Meta title="4.Tasarım" description="www.nesbilgi.com.tr" />
               </Card>
+            </Col>
+          </Row>
+          <Row gutter={16} style={{ marginTop: 10 }}>
+            <Col style={{ textAlign: 'center' }} className="gutter-row">
+              <strong style={{ color: '#de0000' }}>Not:</strong> Özel Tasarım Yaptırmak İçin (0216 688 51 00) Nolu
+              Telefonu Arayarak Satış Temsilcimiz İle Görüşebilirsiniz!
             </Col>
           </Row>
         </div>
